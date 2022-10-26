7f581793f11b9afdc4251324a44829841fa0a8d8
Diogo Santos Ribeiro <diosanto@c1r17s8.42lisboa.com>
2022-10-26 11:31:18 +0100

    asdasd

--- functions/ft_strlcpy.c
+++ functions/ft_strlcpy.c
@@ -6,29 +6,29 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 15:13:53 by diosanto          #+#    #+#             */
-/*   Updated: 2022/10/24 16:07:18 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/10/26 11:30:59 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
-size_t	*ft_strlcpy(char *dest, const char *src, size_t n)
+size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 {
 	size_t	i;
+	size_t	len;
 
+	len = ft_strlen(src);
 	i = 0;
-	if (size > 0)
+	if (n > 0)
 	{
-		while (src[i] && i < (size - 1))
+		while (src[i] && i < (n - 1))
 		{
 			dest[i] = src[i];
 			i++;
 		}
 		dest[i] = 0;
 	}
-	while (src[i])
-	{
-		i++;
-	}
-	return (i);
+	return (len);
 }
