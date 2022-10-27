3dded07fdbc9fbdd4f1d3af3edce9eb762188482
Diogo Santos Ribeiro <diosanto@c1r8s3.42lisboa.com>
2022-10-27 15:28:31 +0100

    asdasd

--- ft_split.c
+++ ft_split.c
@@ -6,30 +6,12 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:14:06 by diosanto          #+#    #+#             */
-/*   Updated: 2022/10/27 14:30:49 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/10/27 15:24:59 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
-
-/*char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*str;
-	size_t	i;
-
-	str = (char *)malloc(sizeof(char) * len);
-	if (!str)
-		return (NULL);
-	while (start < len)
-	{
-		str[i] = s[start];
-		i++;
-		start++;
-	}
-	str[start] = '\0';
-	return (str);
-}*/
 
 int	word_counter(char const *s, char c)
 {
