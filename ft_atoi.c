908d22817c68e2a455f9248285369e6cea1d300d
Diogo Santos Ribeiro <diosanto@c1r16s7.42lisboa.com>
2022-10-28 12:21:51 +0100

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 16:30:08 by diosanto          #+#    #+#             */
-/*   Updated: 2022/10/26 12:20:28 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/10/28 12:06:58 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_atoi(const char *str)
 	{
 		i++;
 	}
-	while (str[i] == '-' || str[i])
+	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
 		{
