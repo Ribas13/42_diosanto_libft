c16f0bfd6024a1cc84aafd8279ad2f191e0e69a1
Ribas13 <diogo10ribeiro.dr@gmail.com>
2022-10-31 12:31:00 +0000

    update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,36 +6,66 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 11:44:25 by diosanto          #+#    #+#             */
-/*   Updated: 2022/10/29 13:43:24 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/10/31 12:29:25 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_check_if0(int n)
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_size(int n)
 {
 	int	size;
 
-	if (n > 0)
+	size = 0;
+	while (n)
 	{
-		size = 0;
+		n /= 10;
+		size++;
 	}
-	else
-		size = 1;
 	return (size);
 }
 
 int	ft_sign(int n)
 {
-	
+	if (n < 0)
+		return (1);
+	return (0);
 }
 
 char	*ft_itoa(int n)
 {
-	int		nbr;
+	long	nbr;
 	int		size;
-	int		sign;
 	char	*str;
+	int		i;
 
 	nbr = n;
-	size = ft_check_if0(n);
-	
+	i = 0;
+	size = ft_size(n);
+	str = (char *)malloc(size + 1);
+	if (!str)
+		return (0);
+	if (n == 0)
+	{
+		str[i] = '0';
+		i++;
+		str[i] = '\0';
+	}
+	while (n > 0)
+	{
+		str[i] = (n % 10) + '0';
+		nbr = nbr / 10;
+		i++;
+	}
+	if (ft_sign(n) == 1)
+		str[i] = '-';
+	str[i] = '\0';
+	return (str);
+}
+
+int	main(void)
+{
+	printf("%i\n", ft_size(25));
+	printf("%s\n", ft_itoa(25));
 }
