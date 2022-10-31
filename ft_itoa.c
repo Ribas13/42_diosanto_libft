d3a12d6fbb612c755e5f8b711ced4c735df8970a
Ribas13 <diogo10ribeiro.dr@gmail.com>
2022-10-31 10:05:31 +0000

    update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,10 +6,36 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 11:44:25 by diosanto          #+#    #+#             */
-/*   Updated: 2022/10/28 12:54:46 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/10/29 13:43:24 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_check_if0(int n)
+{
+	int	size;
+
+	if (n > 0)
+	{
+		size = 0;
+	}
+	else
+		size = 1;
+	return (size);
+}
+
+int	ft_sign(int n)
+{
+	
+}
+
 char	*ft_itoa(int n)
 {
+	int		nbr;
+	int		size;
+	int		sign;
+	char	*str;
+
+	nbr = n;
+	size = ft_check_if0(n);
+	
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 16:02:00 by diosanto          #+#    #+#             */
-/*   Updated: 2022/10/27 15:15:39 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/10/28 17:52:54 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	a;
 
 	i = 0;
-	if (!little)
+	if (little[0] == '\0')
 		return ((char *)big);
 	while (big[i] != '\0')
 	{
 		a = 0;
-		while (big[i + a] == little[a] && i + a < len)
+		while (big[i + a] == little[a] && (i + a) < len)
 		{
 			if (big[i + a] == '\0' && little[a] == '\0')
 			{
@@ -33,7 +33,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 		}
 		if (little[a] == '\0')
 		{
-			return ((char *)big + a);
+			return ((char *)big + i);
 		}
 		i++;
 	}
