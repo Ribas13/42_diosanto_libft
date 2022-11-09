e89fa95add48b2af537294033237f6807aa21d14
Diogo Santos Ribeiro <diosanto@c1r17s8.42lisboa.com>
2022-11-09 17:25:57 +0000

    done

--- Makefile
+++ Makefile
@@ -39,7 +39,10 @@ BONUS 	=	ft_lstnew.c			\
 			ft_lstsize.c		\
 			ft_lstlast.c		\
 			ft_lstadd_back.c	\
-			ft_lstdelone.c
+			ft_lstdelone.c		\
+			ft_lstclear.c		\
+			ft_lstiter.c		\
+			ft_lstmap.c
 
 NAME 	=	libft.a
 OBJECTS	=	$(SOURCES:.c=.o)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 16:07:30 by diosanto          #+#    #+#             */
-/*   Updated: 2022/11/09 15:33:27 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/11/09 16:41:45 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,8 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
