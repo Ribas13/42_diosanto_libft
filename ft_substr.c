9aa9523a588cee9687c97f560a7536ce5570326a
Ribas13 <108695074+Ribas13@users.noreply.github.com>
2022-11-15 09:04:07 +0000

    Add files via upload

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: diosanto <diosanto@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 12:22:37 by diosanto          #+#    #+#             */
-/*   Updated: 2022/11/11 08:49:04 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/11/15 08:58:07 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,42 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	i = 0;
 	j = start;
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
-	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (start > ft_strlen(s))
+	{
+		str = malloc(sizeof(char));
+		*str = 0;
+		return (str);
+	}
+	if (len >= ft_strlen(s))
+		len = ft_strlen(s) - start;
+	str = (char *)malloc(sizeof(char) * len + 1);
 	if (!str)
 		return (NULL);
+	while (j < ft_strlen(s) && i < len)
+		str[i++] = s[j++];
+	str[i] = '\0';
+	return (str);
+}
+
+/*char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	j = start;
+	if (len > ft_strlen(s))
+	{
+		len = ft_strlen(s);
+	}
+	str = (char *)malloc(len + 1);
+	if (!str)
+	{
+		return (NULL);
+	}
 	while (j < ft_strlen(s) && i < len)
 	{
 		str[i] = s[j];
@@ -35,4 +66,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	str[i] = '\0';
 	return (str);
-}
+}*/
